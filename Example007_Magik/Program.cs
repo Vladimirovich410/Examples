6a4bb676d867fc3cc553ceee0c4b1c5f0df503a7
Vladimir <vermakov410@gmail.com>
2023-05-23 02:02:33 +0300

    breikc)

---
+++ Example007_Magik/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+// Console.SetCursorPosition(10, 4);
+// Console.WriteLine("+");
+
+int xa = 10;
+int ya = 10;
+int xb = 10;
+int yb = 30;
+int xc = 32;
+int yc = 30;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("*");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("*");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("*");
